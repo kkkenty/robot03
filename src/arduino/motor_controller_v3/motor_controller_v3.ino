9e627586b8a9e373887c0378c8e1fa1e007b254d
kento <kento@example.com>
2021-09-12 11:37:59 +0900

    add files for gmapping

---
+++ src/arduino/motor_controller_v3/motor_controller_v3.ino
@@ -0,0 +1,54 @@
+// 地図作成用
+#include "motor_control.h"
+#include <ros.h>
+#include "msgs/Motor.h"
+//------------------------------------------------
+//  グローバル変数定義
+//------------------------------------------------
+const float PERIOD = 10.0; // ms -> 1000/PERIOD[Hz]
+msgs::Motor SPEED;
+
+encoder enc_left(PIN_A_LEFT, PIN_B_LEFT);
+encoder enc_right(PIN_A_RIGHT, PIN_B_RIGHT);
+  motor mot_left(PIN_PWM_LEFT, PIN_DIR_LEFT);
+  motor mot_right(PIN_PWM_RIGHT, PIN_DIR_RIGHT);
+
+ros::NodeHandle nh;
+ros::Publisher pub("speed", &SPEED);
+
+void controlCb(const msgs::Motor& get_msg){
+  mot_left.PWM = (int)get_msg.left;
+  mot_right.PWM = (int)get_msg.right;
+}
+void directCb(const msgs::Motor& get_msg){
+  mot_left.PWM = (int)get_msg.left;
+  mot_right.PWM = (int)get_msg.right;
+}
+
+ros::Subscriber<msgs::Motor> sub("control", &controlCb);
+
+void setup() {
+  Serial.begin(57600);
+  attachInterrupt(3, counter0, CHANGE);
+  attachInterrupt(2, counter0, CHANGE);
+  attachInterrupt(0, counter1, CHANGE);
+  attachInterrupt(1, counter1, CHANGE);
+  nh.initNode();
+  nh.advertise(pub);
+  nh.subscribe(sub);
+}
+
+void loop() {
+  // 現在の速度の取得
+  enc_left.getSPEED(0);
+  enc_right.getSPEED(1);
+  SPEED.left = SPEED_NOW[0];
+  SPEED.right = SPEED_NOW[1];
+  // 速度の出力
+  mot_left.Write();
+  mot_right.Write();
+  // ROS処理
+  pub.publish(&SPEED);
+  nh.spinOnce();
+  delay(PERIOD);
+}
---
+++ src/create_map/src/controller4.cpp
@@ -0,0 +1,117 @@
+// joystickから目標速度、Arduinoから現在速度をsubし、PWM値をpubする
+// map作成用
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <msgs/Motor.h>
+#include <msgs/PID.h>
+//-------------------
+// global変数
+//-------------------
+msgs::Motor control;
+msgs::PID param;
+const float LIMIT = 0.15, ACC = 1;
+float kp = 25.0, ki = 2.0, kd = 0.1; 
+double v_goal = 0.0, w_goal = 0.0, kv = 0.458, kw = 0.739, d = 0.31;
+int state = 0, FRIQUENCE = 20;
+//------------------
+// クラスの定義
+//------------------
+class feedback
+{
+  private:
+    float SPEED_SUM = 0.0, SPEED_ACC = 0.0, SPEED_ERROR = 0.0, SPEED_ERROR_PRE = 0.0, SPEED_PRE = 0.0;
+  public:
+    float PWM = 0.0, SPEED_GOAL = 0.0, SPEED_NOW = 0.0;
+    float PID(); // PID制御でPWM計算
+    float daikei(); // 台形制御でPWM計算
+};
+float feedback::PID(){
+  SPEED_ERROR = SPEED_GOAL - SPEED_NOW; // P項 or 偏差
+  SPEED_SUM += (SPEED_ERROR + SPEED_ERROR_PRE) / 2.0; // I項
+  SPEED_ACC = (float)(SPEED_NOW - SPEED_PRE) * (float)FRIQUENCE; // D項
+  //ROS_INFO("lf", SPEED_ACC);
+  param.p = kp * SPEED_ERROR;
+  param.i = ki * ki * SPEED_SUM;
+  param.d = kd * SPEED_ACC;
+  PWM = param.p + param.i - param.d;
+  SPEED_ERROR_PRE = SPEED_ERROR;
+  SPEED_PRE = SPEED_NOW;
+  return PWM;
+}
+float feedback::daikei(){
+  SPEED_ERROR = SPEED_GOAL - SPEED_NOW; // P項 or 偏差
+  if(SPEED_ERROR > LIMIT){
+    PWM += ACC;
+  }
+  else if(SPEED_ERROR < -LIMIT){
+    PWM -= ACC;
+  }
+  else{ // -LIMIT <= SPEED_ERROR[i] <= LIMIT
+  }
+  return PWM;
+}
+// -----------------
+// クラスの宣言
+//------------------
+feedback LEFT, RIGHT;
+//------------------
+// CallBack関数の定義
+//------------------
+void joyCb(const sensor_msgs::Joy &joy_msg)
+{
+  v_goal = kv * joy_msg.axes[1]; w_goal = kw * joy_msg.axes[3];
+  LEFT.SPEED_GOAL  = v_goal - (w_goal * d);
+  RIGHT.SPEED_GOAL = v_goal + (w_goal * d);
+  if(joy_msg.buttons[0]){
+    state++;
+    if(state % 2 == 1){
+      ROS_INFO("STOPPING!");
+    }
+    else if(state % 2 == 0){
+      ROS_INFO("RESTARTING!");
+    }
+  }
+  //ROS_INFO("LEFT.SPEED_GOAL:%lf", (double)PWM.L);
+  //ROS_INFO("RIGHT.SPEED_GOAL:%lf", (double)PWM.R);
+}
+void ardCb(const msgs::Motor &sensor)
+{
+  LEFT.SPEED_NOW  = sensor.left;
+  RIGHT.SPEED_NOW = sensor.right;
+}
+//---------------------
+// main関数
+//---------------------
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "controller4");
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+  pnh.getParam("kv", kv);
+  pnh.getParam("kw", kw);
+  pnh.getParam("d", d);
+  pnh.getParam("kp", kp);
+  pnh.getParam("ki", ki);
+  pnh.getParam("kd", kd);
+  pnh.getParam("FRIQUENCE", FRIQUENCE);
+  ros::Subscriber joy_sub = nh.subscribe("joy", 10, joyCb);
+  ros::Subscriber ard_sub = nh.subscribe("speed", 1, ardCb);	
+  ros::Publisher ard_pub = nh.advertise<msgs::Motor>("cmd_pwm", 1);
+  ros::Publisher scr_pub = nh.advertise<msgs::PID>("param", 1);
+  ros::Rate loop_rate(FRIQUENCE);
+  
+  while (ros::ok())
+  {
+    control.left  = LEFT.PID();
+    control.right = RIGHT.PID();
+    if(state % 2 == 1){
+      control.left  = 0.0;
+      control.right = 0.0;
+    }
+    ard_pub.publish(control);
+    scr_pub.publish(param);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
