62a2d06bdb19b9c2c315d73e28f469760086c0a4
kento <kento@example.com>
2021-11-05 20:39:09 +0900

    change from robot03_navigation to navi

--- src/navi/src/pure_pursuit_loop_v2.cpp
+++ src/navi/src/pure_pursuit_loop_v2.cpp
@@ -8,80 +8,19 @@
 
 // global変数 // 
 const int pt = 5; //目標地点の個数
-// sister's room
-//double goal[pt][2] = {{-0.5, -0.2}, {1.0, -0.2}, {1.0, -1.2}, {-0.5, -1.2}, {-0.5, -0.2}}; 
-// robosa
-double goal[pt][2] = {{0.3, -0.6}, {4.0, -0.6}, {4.0, -2.1}, {0.3, -2.1}, {0.3, -0.6}}; 
-int FRIQUENCE = 20, den = 100, ahed = 5, stap = 0; // 経路分割数、lookaheddistance、停止変数
+double goal[pt][2] = {{0.3, -0.6}, {4.0, -0.6}, {4.0, -2.1}, {0.3, -2.1}, {0.3, -0.6}}; // robosa
+//double goal[pt][2] = {{-0.5, -0.2}, {1.0, -0.2}, {1.0, -1.2}, {-0.5, -1.2}, {-0.5, -0.2}}; // sister's room
+int stap = 0; // 停止変数
 double vel = 0.35; // ロボットの速度
 
-// 第1,2引数と第3,4引数の点間距離を算出
-double dis(double x, double y, double ax, double ay){
-  return sqrt(pow(ax-x, 2) + pow(ay-y, 2));
-}
+// 第1,2引数と第3,4引数の点間距離を算出 //
+double dis(double x, double y, double ax, double ay);
 
-// 最も近い点を選択(局所解は無いと仮定)
-// 第1,2引数：現在座標、第3,4引数：現在の経路番号、位置(参照渡し)
-int dismin(const double &x, const double &y, int &sum, double dotpath[][2]){
-  static int nowpose = 0, lastpose = 0;
-  int i, mode = 0, count = 0; // 計算状態変数
-  double mindis, nowdis; // 最小経路値、現在経路値
+// 最も近い点を選択(局所解は無いと仮定) //
+int dismin(const double &x, const double &y, int &sum, double dotpath[][2]);
 
-  while(count <= sum){ // 全探索しない限り周回
-    for(i=lastpose;i<sum;i++){ // 目標経路の更新
-      nowdis = dis(x, y, dotpath[i][0], dotpath[i][1]);
-      count++;
-      if(mode == 0){ // 初期化
-        mindis = nowdis;
-        nowpose = i;
-        mode = 1;
-        continue;
-      }
-      if(mindis > nowdis){ // 最短距離の更新
-        mindis = nowdis;
-        nowpose = i;
-        mode = 2;
-      }
-      else if(mode == 2){ // 最短経路の更新が途絶えたら終了
-        lastpose = nowpose; // 現在の位置を保持
-        return nowpose;
-      }
-      if(count > sum){ // 初期位置が最短の場合
-        lastpose = nowpose; // 現在の位置を保持
-        return nowpose;
-      }
-    }
-    lastpose = 0;
-  }
-}
-
-void joyCb(const sensor_msgs::Joy &joy_msg)
-{
-  if(joy_msg.buttons[3]){
-    if(stap == 0){
-      ROS_INFO("NAVI STOPPING!");
-      stap = 1;
-    }
-    else if(stap == 1){
-      ROS_INFO("NAVI RESTARTING!");
-      stap = 0;
-    }
-  }
-  if(joy_msg.buttons[4]){
-    vel += 0.05;
-    if(vel > 0.45){
-      vel = 0.45;
-    }
-    ROS_INFO("Now, velocity is [%lf]", vel);
-  }
-  if(joy_msg.buttons[5]){
-    vel -= 0.05;
-    if(vel < 0.05){
-      vel = 0.05;
-    }
-    ROS_INFO("Now, velocity is [%lf]", vel);
-  }
-}
+// joyのcallback関数 //
+void joyCb(const sensor_msgs::Joy &joy_msg);
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "pure_pursuit_loop_v2");
@@ -90,6 +29,7 @@ int main(int argc, char** argv){
   
   tf::StampedTransform tf;
   geometry_msgs::Twist cmd;
+  int FRIQUENCE = 20, den = 100, ahed = 5; // 経路分割数、lookaheddistance
   pnh.getParam("FRIQUENCE", FRIQUENCE);
   pnh.getParam("den", den);
   pnh.getParam("ahed", ahed);
@@ -98,7 +38,6 @@ int main(int argc, char** argv){
   int i, j, k;
   double x = 0.0, y = 0.0, yaw = 0.0; // robot's pose
   double alpha = 0.0, L = 0.0; // 方位誤差、距離
-  static int cr = 0, ad = 0, gcr = 0, gad = 0; // 現在の経路番号、位置、目標店の経路番号、位置
   
   // Markerの定義 //
   visualization_msgs::Marker points, npoint, gpoint;
@@ -150,7 +89,7 @@ int main(int argc, char** argv){
     rate.sleep();
     ros::spinOnce();
     
-    // 点Markerの初期化(もっと良い初期化の方法は無いかな...)
+    // gpoint, npointの初期化(もっと良い初期化の方法は無いかな...) //
     visualization_msgs::Marker npoint, gpoint;
     npoint.header.frame_id = gpoint.header.frame_id = "map";
     npoint.header.stamp = gpoint.header.stamp = ros::Time::now();
@@ -163,7 +102,7 @@ int main(int argc, char** argv){
     npoint.color.g = 1.0;  npoint.color.a = 1.0;
     gpoint.color.r = 1.0;  gpoint.color.a = 1.0;
     
-    // ロボット座標の取得
+    // ロボット座標の取得 //
     try{
       listener.lookupTransform("/map", "/base_link", ros::Time(0), tf);
       x = tf.getOrigin().x();
@@ -176,14 +115,14 @@ int main(int argc, char** argv){
       continue;
     }
     
-    // 最も近い点を選択
+    // 最も近い点を選択 //
     static int pose = 0;
     pose = dismin(x, y, sum, dotpath);
     p.x = dotpath[pose][0];
     p.y = dotpath[pose][1]; 
     npoint.points.push_back(p);
     
-    // 目標点の設定
+    // 目標点の設定 //
     pose += ahed;
     if(pose >= sum){ // poseの繰り上げ
       pose -= sum;
@@ -192,11 +131,11 @@ int main(int argc, char** argv){
     p.y = dotpath[pose][1];
     gpoint.points.push_back(p);
     
-    // 目標点との相対的な角度、距離の算出
+    // 目標点との相対的な角度、距離の算出 //
     alpha = atan2(dotpath[pose][1] - y, dotpath[pose][0] - x) - yaw;
     L = dis(x, y, dotpath[pose][0], dotpath[pose][1]);
     
-    // 車速と角速度の算出
+    // 車速と角速度の算出 //
     cmd.linear.x = vel;
     cmd.angular.z = 2.0 * vel * sin(alpha) / L;
     if(stap){ // naviの停止コマンド
@@ -204,11 +143,77 @@ int main(int argc, char** argv){
       cmd.angular.z = 0.0;
     }
     
-    // 車速の配信
+    // 車速の配信 //
     cmd_pub.publish(cmd);
     marker_pub.publish(points);
     marker_pub.publish(npoint);
     marker_pub.publish(gpoint);
   }
   return 0;
+}
+
+// 第1,2引数と第3,4引数の点間距離を算出 //
+double dis(double x, double y, double ax, double ay){
+  return sqrt(pow(ax-x, 2) + pow(ay-y, 2));
+}
+// 最も近い点を選択(局所解は無いと仮定) //
+int dismin(const double &x, const double &y, int &sum, double dotpath[][2]){
+  static int nowpose = 0, lastpose = 0;
+  int i, mode = 0, count = 0; // 計算状態変数
+  double mindis, nowdis; // 最小経路値、現在経路値
+
+  while(count <= sum){ // 全探索しない限り周回
+    for(i=lastpose;i<sum;i++){ // 目標経路の更新
+      nowdis = dis(x, y, dotpath[i][0], dotpath[i][1]);
+      count++;
+      if(mode == 0){ // 初期化
+        mindis = nowdis;
+        nowpose = i;
+        mode = 1;
+        continue;
+      }
+      if(mindis > nowdis){ // 最短距離の更新
+        mindis = nowdis;
+        nowpose = i;
+        mode = 2;
+      }
+      else if(mode == 2){ // 最短経路の更新が途絶えたら終了
+        lastpose = nowpose; // 現在の位置を保持
+        return nowpose;
+      }
+      if(count > sum){ // 初期位置が最短の場合
+        lastpose = nowpose; // 現在の位置を保持
+        return nowpose;
+      }
+    }
+    lastpose = 0;
+  }
+}
+// joyのcallback関数 //
+void joyCb(const sensor_msgs::Joy &joy_msg)
+{
+  if(joy_msg.buttons[3]){
+    if(stap == 0){
+      ROS_INFO("NAVI STOPPING!");
+      stap = 1;
+    }
+    else if(stap == 1){
+      ROS_INFO("NAVI RESTARTING!");
+      stap = 0;
+    }
+  }
+  if(joy_msg.buttons[4]){
+    vel += 0.05;
+    if(vel > 0.45){
+      vel = 0.45;
+    }
+    ROS_INFO("Now, velocity is [%lf]", vel);
+  }
+  if(joy_msg.buttons[5]){
+    vel -= 0.05;
+    if(vel < 0.05){
+      vel = 0.05;
+    }
+    ROS_INFO("Now, velocity is [%lf]", vel);
+  }
 }
